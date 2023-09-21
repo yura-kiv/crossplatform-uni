03f4d88599f536f98275a0c1e862fdd9b3184f94
Kivlyuk Yura <kivlyukyura@gmail.com>
2023-09-22 00:17:37 +0300

    .

--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -1,2 +1,124 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+// VAR - 62, Kivlyuk Yuriy IPZ-41
+//
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    const string INPUT_FILE = "../../../input.txt";
+    const string OUTPUT_FILE = "../../../output.txt";
+
+    static bool IsValidInput(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("You cannot enter an empty line into a program.");
+            return false;
+        }
+
+        if (!Regex.IsMatch(input, "^[a-zA-Z0-9]+$"))
+        {
+            Console.WriteLine("One or both lines contain invalid characters.");
+            return false;
+        }
+
+        return true;
+    }
+
+    static int CheckStrInStr(string str1, string str2)
+    {
+        int index = str1.IndexOf(str2);
+
+        if (index == -1)
+            return 0;
+
+        str1 = str1.Substring(0, 1).ToUpper() + str1.Substring(1);
+        str1 = str1.Remove(index, 1).Insert(index, str1[index].ToString().ToUpper());
+
+        Console.Write(str1);
+
+        return 1;
+    }
+
+    static int CalcStrInStr(string str1, string str2)
+    {
+        int str1Lenght = str1.Length;
+        int str2Lenght = str2.Length;
+        bool flag;
+
+        for (int j = 1; j < str1Lenght; j++)
+        {
+            flag = true;
+            for (int i = 0; (i < str1Lenght - j) && (i < str2Lenght); i++)
+            {
+                if (str1[j + i] != str2[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                return j;
+        }
+        return str1Lenght;
+    }
+
+    static string GetResult(string str1, string str2, int index)
+    {
+        str1 = str1.Substring(0, 1).ToUpper() + str1.Substring(1);
+        str2 = str2.Substring(0, 1).ToUpper() + str2.Substring(1);
+
+        str1 = str1.Remove(index);
+
+        Console.Write(str1);
+        Console.Write(str2);
+
+        return str1 + str2;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(INPUT_FILE))
+            {
+                using (StreamWriter writer = new StreamWriter(OUTPUT_FILE))
+                {
+                    string name1 = reader.ReadLine();
+                    string name2 = reader.ReadLine();
+
+                    if (!IsValidInput(name1) || !IsValidInput(name2))
+                    {
+                        return;
+                    }
+                       
+                    string name1Lower = name1.ToLower();
+                    string name2Lower = name2.ToLower();
+
+                    if (CheckStrInStr(name1Lower, name2Lower) == 1 || CheckStrInStr(name2Lower, name1Lower) == 1)
+                        return;
+
+                    int i1 = CalcStrInStr(name1Lower, name2Lower);
+                    int i2 = CalcStrInStr(name2Lower, name1Lower);
+
+                    if (i1 < i2)
+                    {
+                        string result = GetResult(name1Lower, name2Lower, i1);
+                        writer.Write(result);
+                    }
+                    else
+                    {
+                        string result = GetResult(name2Lower, name1Lower, i2);
+                        writer.Write(result);
+                    }
+                }
+            }
+        }
+        catch {
+            Console.WriteLine("There was a problem reading the file..");
+        }
+    }
+}
+
---
+++ lab_2/Program.cs
@@ -0,0 +1,56 @@
+﻿//
+// VAR - 62, Kivlyuk Yuriy IPZ-41
+//
+class Program
+{
+    const string INPUT_FILE = "../../../input.txt";
+    const string OUTPUT_FILE = "../../../output.txt";
+
+    static int getDepthFromFile()
+    {
+        int depth = -1;
+        try
+        {
+            string inputText = File.ReadAllText(INPUT_FILE);
+            if (int.TryParse(inputText, out depth) && depth >= 0 && depth <= 100)
+            {
+                return depth;
+            }
+            else
+            {
+                Console.WriteLine("Invalid value in file. Only integer values between 0 and 100 are valid.");
+                return -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("There was a problem reading the file..");
+            return -1;
+        }
+    }
+
+    static void GetSequence(int start, int end, int depth, List<int> sequence)
+    {
+        if (depth == 0)
+        {
+            sequence.Add(start);
+            return;
+        }
+        GetSequence(start, start + end, depth - 1, sequence);
+        GetSequence(start + end, start, depth - 1, sequence);
+    }
+
+    static void Main()
+    {
+        int depth = getDepthFromFile();
+        if (depth == -1) return;
+        Console.WriteLine("Depth: " + depth);
+
+        List<int> sequence = new List<int>();
+        GetSequence(1, 1, depth, sequence);
+
+        int sequenceSum = sequence.Sum() + 1;
+        Console.WriteLine("Sum: " + sequenceSum);
+        File.WriteAllText(OUTPUT_FILE, sequenceSum.ToString());
+    }
+}
